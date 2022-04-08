0abdbe80ee3a32246cb97558b9fbe9408efbeb25
olgabs <93311817+olgabs@users.noreply.github.com>
2022-04-07 23:34:22 -0300

    Create TesteAviao.java

---
+++ TesteAviao.java
@@ -0,0 +1,20 @@
+public class TesteAviao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	
+	
+	Aviao Aviao = new Aviao("Boing_473","Passageiros",2016,"Gol");
+	
+	
+	Aviao.Info_Aviao();
+	
+	
+	
+	
+	
+	}
+
+}
